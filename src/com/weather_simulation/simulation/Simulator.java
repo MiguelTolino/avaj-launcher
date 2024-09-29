712644c01c4fc322dbd2402f0d054736ad0dbe90
mmateo-t <mmateo-t@student.42madrid.com>
2024-09-29 15:08:10 +0200

    Refactor package imports and update Simulator class and related files

--- src/com/weather_simulation/simulation/Simulator.java
+++ src/com/weather_simulation/simulation/Simulator.java
@@ -77,4 +77,18 @@ public class Simulator {
       }
     }
   }
+
+  public static void main(String[] args) {
+    try {
+      if (args.length != 2) {
+        System.out.println("Usage: java Simulator <filename>");
+        System.exit(1);
+      }
+      Simulator sim = new Simulator();
+      sim.setScenario(args[0]);
+      sim.runSimulation();
+    } catch (Exception e) {
+      System.out.println("Error: " + e.getMessage());
+    }
+  }
 }
